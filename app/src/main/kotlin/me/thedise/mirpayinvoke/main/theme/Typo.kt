1870041b183837018a9ab360b29040d99b8bc226
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-02-09 21:10:45 +0300

    Deleted non free fonts

--- app/src/main/kotlin/me/thedise/mirpayinvoke/main/theme/Typo.kt
+++
@@ -1,9 +0,0 @@
-package me.thedise.mirpayinvoke.main.theme
-
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import me.thedise.mirpayinvoke.R
-
-val googleSans = FontFamily(
-    Font(R.font.google_sans_regular)
-)
