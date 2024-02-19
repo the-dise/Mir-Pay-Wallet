942221aecd9bd2e49ca4eaa5bfe0ba83fc580829
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-02-19 10:27:19 +0300

    Added support for rotary scrolling, position indicator, time text

--- app/src/main/kotlin/me/thedise/mirpayinvoke/ui/SettingsScreen.kt
+++ app/src/main/kotlin/me/thedise/mirpayinvoke/ui/SettingsScreen.kt
@@ -1,13 +1,21 @@
 package me.thedise.mirpayinvoke.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
 import androidx.wear.compose.material.ListHeader
+import androidx.wear.compose.material.PositionIndicator
 import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
 import androidx.wear.compose.material.Vignette
 import androidx.wear.compose.material.VignettePosition
+import androidx.wear.compose.material.scrollAway
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.compose.rotaryinput.rotaryWithScroll
 import me.thedise.mirpayinvoke.R
 import me.thedise.mirpayinvoke.common.Card
 import me.thedise.mirpayinvoke.ui.theme.MirPayTheme
@@ -15,6 +23,7 @@ import me.thedise.mirpayinvoke.ui.widgets.cardChip
 import me.thedise.mirpayinvoke.ui.widgets.hapticChip
 import me.thedise.mirpayinvoke.ui.widgets.timerChip
 
+@OptIn(ExperimentalHorologistApi::class)
 @Composable
 fun SettingsScreen(
     timerTicks: Int,
@@ -25,11 +34,21 @@ fun SettingsScreen(
     onToggleHapticAction: (Boolean) -> Unit
 ) {
     MirPayTheme {
+        val state = rememberScalingLazyListState()
 
-        Scaffold(vignette = {
+        Scaffold(positionIndicator = {
+            PositionIndicator(state)
+        }, vignette = {
             Vignette(vignettePosition = VignettePosition.TopAndBottom)
+        }, timeText = {
+            TimeText(modifier = Modifier.scrollAway(state))
         }) {
-            ScalingLazyColumn {
+            ScalingLazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .rotaryWithScroll(scrollableState = state),
+                state = state,
+            ) {
                 item {
                     ListHeader {
                         Text(
